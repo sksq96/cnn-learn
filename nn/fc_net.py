5ad91b73c4405eeb67eb0d5344a5cf1a1df853e5
Shubham Chandel <shubham.zeez@gmail.com>
2017-01-29 20:54:39 +0530

    Fully Connected Net,

---
+++ nn/fc_net.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: shubham
+# @Date:   2017-01-29 20:41:19
+# @Last Modified by:   shubham
+# @Last Modified time: 2017-01-29 20:47:43
+
+import numpy as np
+
+from util.layers import *
+from util.layer_utils import *
+
